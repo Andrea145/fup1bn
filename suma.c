c1538726c32e962d9fbef3a6689d71ebd7f696e1
36c <mari_36is@hotmail.com>
2015-10-28 20:57:40 -0600

    andy

---
+++ suma.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+/* Programa: Multiplicación con sumas sucesivas */
+
+int main()
+{
+    int acumulador, contador, n1, n2;
+
+    printf( "\n   Introduzca primer n%cmero (entero): ", 163 );
+    scanf( "%d", &n1 );
+    printf( "\n   Introduzca segundo n%cmero (entero): ", 163 );
+    scanf( "%d", &n2 );
+
+    if ( n1 >= 0 && n2 >= 0 )
+    {
+        acumulador = 0;
+
+        if ( n2 != 0 )
+        {
+            contador = 1;
+            while ( contador <= n1 )
+            {
+                acumulador += n2;
+                contador++;
+            }
+        }
+
+        printf( "\n   %d * %d = %d", n1, n2, acumulador );
+
+    }
+    else
+        printf( "\n   ERROR: Ambos n%cmeros deben ser mayores o iguales que cero.", 163 );
+
+    
+
+    return 0;
+}
+
